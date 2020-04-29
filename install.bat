70bda78aeb6f72fc93276027a7fc4217a9e5d9e9
tom <tom@sapletta.com>
2020-04-29 13:34:20 +0200

    update docs

--- install.bat
+++ install.bat
@@ -1,5 +1,5 @@
 ::py -m pip install --user virtualenv
-py -m pip install virtualenv
-py -m venv env
+py pip install virtualenv
+py venv env
 .\env\Scripts\activate
-py -m pip install -r src/requirements.txt
+pip install -r src/requirements.txt
